778706ca0d8298e6b41274b84add39ae8708cdfa
Dexaf <federicopisajob@gmail.com>
2024-01-09 23:46:46 +0100

    fixed async on writefile promise

--- src/utils/crashHandlingRoutine.ts
+++ src/utils/crashHandlingRoutine.ts
@@ -1,8 +1,8 @@
 import { promises as fsPromises } from 'fs';
 
-export default (error: Error) => {
+export default async (error: Error) => {
   try {
-    fsPromises.writeFile('errorLog.txt', error.message + '\n' + error.stack);
+    await fsPromises.writeFile('errorLog.txt', error.message + '\n' + error.stack);
   } catch (error) {
     console.error("shit hitted the fan m8");
   }
